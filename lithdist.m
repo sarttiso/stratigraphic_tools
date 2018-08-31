3275ac8f9c00f981901cd00a243f47d142231217
Adrian Tasistro-Hart <adrian@tasistro.com>
2018-08-31 18:55:36 +0200

    added ability for user to specify distance above or below bed of given lithology rather than just smallest distance to bed

--- lithdist.m
+++ lithdist.m
@@ -9,13 +9,18 @@
 % lithcmp: Lithology against which to compare, must be a member of lith
 % samples: vector of heights for which to compute distance to bed of given
 %   lithology
-% min_thickness: Minimum thicknesso of a bed of the given lithology for
-% which to compute the distance to
+% 'min_thickness': Minimum thickness of a bed of the given lithology for
+%   which to compute the distance to
+% 'sided': (default 'both') Compute minimum distance to top, bottom, or
+%   either of a bed of a given lithology; can be 'top','bottom','both'
 %
 % OUT:
 % dist: vector of distances, one distance for each member of samples. a
 %   distance of zero indicates that a queried height is actually in a bed
-%   of given lithology
+%   of given lithology (if user specified 'both' for 'sided'). NaNs may
+%   occur if a user requests a sample height above a bed of a given
+%   lihology when they specified 'bottom', or if they request a sample
+%   height below a bed of a given lithology when they specified 'top'
 %
 % TO DO:
 % - allow for computing distance from arbitrarily many lithologies
@@ -33,6 +38,7 @@ addRequired(parser,'lith',@(x) isnumeric(x) || iscell(x))
 addRequired(parser,'lithcmp',@(x) isnumeric(x) || ischar(x))
 addRequired(parser,'samples',@isnumeric)
 addParameter(parser,'min_thickness',0,validScalarPosNum)
+addParameter(parser,'sided','both',@ischar)
 
 parse(parser,bedthick,lith,lithcmp,samples,varargin{:})
 
@@ -41,6 +47,7 @@ lith = parser.Results.lith;
 lithcmp = parser.Results.lithcmp;
 samples = parser.Results.samples;
 minthick = parser.Results.min_thickness;
+side = parser.Results.sided;
 
 % validate beds and lith
 assert(length(bedthick) == length(lith),...
@@ -52,6 +59,9 @@ bedthick = bedthick(:);
 % validate lithcmp
 assert(ismember(lithcmp,lith),...
     'requested lithology must be present in vector of lithologies (lith)')
+
+% validate side
+side = validatestring(side,{'both','top','bottom'});
 
 % if lithologies given as strings, encode as integers
 if iscell(lith)
@@ -103,14 +113,25 @@ distlb = repmat(lb,1,nsamples)-repmat(samples',nbedslith,1);
 % between them will be positive, so we can ignore negative distances
 distlb(distlb < 0) = NaN;
 
-% create distance vector by taking minimum of all distances for each sample
-% height from both distlb and distub
-dist = min([min(distub);min(distlb)])';
-
-% if a sample is in a bed of the lithology of interest, then it will have a
-% corresponding nan in both distlb and distub, so we need to correct for
-% these points
-inlithidx = logical(sum(isnan(distlb) & isnan(distub)))';
-dist(inlithidx) = 0;
+% return a distance based on what user wants (closest to top, bottom, or 
+% either of a bed of certain lithology)
+switch side
+    case 'both'
+        % create distance vector by taking minimum of all distances for each sample
+        % height from both distlb and distub
+        dist = min([min(distub);min(distlb)])';
+        % if a sample is in a bed of the lithology of interest, then it will have a
+        % corresponding nan in both distlb and distub, so we need to correct for
+        % these points
+        inlithidx = logical(sum(isnan(distlb) & isnan(distub)))';
+        dist(inlithidx) = 0;
+        
+    case 'top'
+        dist = min(distub)';
+        
+    case 'bottom'
+        dist = min(distlb)';
+        
+end
 
 end
